0930429c9bd7950bbb909f1065cd3e98db3bb01b
Mark Basham <mark.basham@diamond.ac.uk>
2011-12-19 10:45:19 +0000

    SCISOFT - Updated Headers

--- uk.ac.diamond.CBFlib/src/uk/ac/diamond/CBFlib/CBFlib.java
+++ uk.ac.diamond.CBFlib/src/uk/ac/diamond/CBFlib/CBFlib.java
@@ -1,21 +1,21 @@
 /*-
- * Copyright © 2009 Diamond Light Source Ltd.
- *
- * This file is part of GDA.
- *
- * GDA is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License version 3 as published by the Free
- * Software Foundation.
- *
- * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along
- * with GDA. If not, see <http://www.gnu.org/licenses/>.
- */
-
+* Copyright © 2011 Diamond Light Source Ltd.
+* Contact :  ScientificSoftware@diamond.ac.uk
+*
+* File id : CBFlib.java
+*
+* This is free software: you can redistribute it and/or modify it under the
+* terms of the GNU General Public License version 3 as published by the Free
+* Software Foundation.
+*
+* This software is distributed in the hope that it will be useful, but 
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this software. If not, see <http://www.gnu.org/licenses/>.
+*/
 package uk.ac.diamond.CBFlib;
 
 /**
